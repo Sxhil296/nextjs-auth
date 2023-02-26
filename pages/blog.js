3e066f27320291f596a8a8c8e62327197dd3de42
Sahil <leviack629@gmail.com>
2023-02-26 10:06:00 +0530

    secure pages

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,33 @@
+import { getSession, useSession } from 'next-auth/react'
 import React from 'react'
 
-const blog = () => {
+
+const blog = ({ data }) => {
+  const { data: session } = useSession();
+  console.log({session})
   return (
     <div>
-        <h1>Blog Page</h1>
+        <h1>Blog Page - {data}</h1>
     </div>
   )
 }
 
 export default blog
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  if(!session) {
+    return{
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanent: false,
+      }
+    }
+  }
+  return {
+    props: {
+      session,
+      data: session ? 'List of 100 personalized blogs' : 'List of free blogs',
+    },
+  }
+}
