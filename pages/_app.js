cfcc9342a119b27b064811fb5dba8125e21c613d
Sahil <leviack629@gmail.com>
2023-02-26 08:11:15 +0530

    github auth

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import '../components/Navbar.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+    <Navbar />
+    <Component {...pageProps} />
+    </>
+  )
+
+}
+
+export default MyApp
